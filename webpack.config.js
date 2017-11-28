4baaa9cdf752ed5034bbf03e25d15e250b9a5ddc
hejny <me@pavolhejny.com>
2017-11-28 15:39:55 +0100

    +! Webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    entry: {
+        first: __dirname + '/src/index'
+    },
+    output: {
+        filename: 'gridbuilding.js',
+        path: __dirname + "/dist/"
+    },
+
+
+    resolve: {
+        // Add '.ts' and '.tsx' as a resolvable extension.
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+    module: {
+        loaders: [
+            // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
+}
